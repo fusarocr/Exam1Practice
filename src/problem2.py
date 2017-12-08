a0a1f892ecdf1efd6ab84470f0996b80f1f47349
fusarocr <fusarocr@rose-hulman.edu>
2017-12-08 18:39:27 -0500

    2b

--- src/problem2.py
+++ src/problem2.py
@@ -193,7 +193,7 @@ def problem2b(rectangle, n, delta, win):
       :type win:    rg.RoseWindow
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # Done: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # ------------------------------------------------------------------
     # ------------------------------------------------------------------
